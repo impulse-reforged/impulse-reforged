156419c296850afa6e2b0ad678bdb2dd0418e9c7
Riggs <riggs9162@gmx.de>
2025-11-29 00:24:54 +0100

    sv_cleanup.lua updated

--- plugins/ops/sv_cleanup.lua
+++ plugins/ops/sv_cleanup.lua
@@ -1,29 +1,38 @@
 impulse.Ops = impulse.Ops or {}
 
 function impulse.Ops.CleanupPlayer(client)
+    local count = 0
     for v, k in ents.Iterator() do
         local owner = k:CPPIGetOwner()
 
         if owner == client then
             if not k.IsBuyable then
+                count = count + 1
                 k:Remove()
             end
         end
     end
+    print("[ops] Cleaned up "..count.." entities for "..client:Name().." ("..client:SteamID64()..")")
 end
 
 function impulse.Ops.CleanupAll()
+    local count = 0
     for v, k in ents.Iterator() do
         local owner = k:CPPIGetOwner()
 
         if owner and !k.IsBuyable then
+            count = count + 1
             k:Remove()
         end
     end
+    print("[ops] Cleaned up "..count.." total entities from the map")
 end
 
 function impulse.Ops.ClearDecals()
+    local count = 0
     for v, k in player.Iterator() do
         k:ConCommand("r_cleardecals")
+        count = count + 1
     end
+    print("[ops] Cleared decals for "..count.." players")
 end
