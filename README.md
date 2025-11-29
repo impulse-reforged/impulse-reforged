91a5d6b4712cf570fb8e7c489a4136156f77111a
Riggs <riggs9162@gmx.de>
2025-11-29 02:32:06 +0100

    feat(ops): add disconnect and spawn messages for lead admins

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ![impulse reforged logo](https://raw.githubusercontent.com/riggs9162/impulse-reforged/refs/heads/main/impulse-reforged-blue.png)
+
 ----
+
 ## impulse: reforged
 impulse reforged is a free, open-source framework for Garry's Mod built upon the original impulse framework by vin. impulse is designed to be a lightweight, efficient, and secure framework for Garry's Mod servers. Alongside this, it's built with performance in mind, and is designed to be as efficient as possible.
 
--- plugins/ops/sv_hooks.lua
+++ plugins/ops/sv_hooks.lua
@@ -1,3 +1,20 @@
 function PLUGIN:PlayerShouldGetHungry(client)
     if client:IsAdmin() and client:GetMoveType() == MOVETYPE_NOCLIP then return false end
 end
+
+gameevent.Listen("player_disconnect")
+hook.Add("player_disconnect", "impulse_ops_disconnect", function(data)
+    for _, target in player.Iterator() do
+        if ( target:IsLeadAdmin() ) then
+            target:AddChatText(Color(135, 206, 235), "[ops] " .. data.name .. " has disconnected from the server. (" .. data.reason .. ")")
+        end
+    end
+end)
+
+hook.Add("PlayerInitialSpawn", "impulse_ops_spawnmsg", function(client)
+    for _, target in player.Iterator() do
+        if ( target:IsLeadAdmin() ) then
+            target:AddChatText(Color(135, 206, 235), "[ops] " .. client:SteamName() .. " has connected to the server.")
+        end
+    end
+end)
