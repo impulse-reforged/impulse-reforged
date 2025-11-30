0d324ebc4edb26c64e4ecd6a016c32c410ae5b97
Riggs <riggs9162@gmx.de>
2025-11-30 01:40:01 +0100

    fix(medical): simplify leg break logic and improve state management
    refactored the leg break and fix methods to streamline the handling
    of broken leg states, removing unnecessary variables and ensuring
    consistent state updates.

--- gamemode/core/libs/sh_medical.lua
+++ gamemode/core/libs/sh_medical.lua
@@ -6,5 +6,11 @@ local PLAYER = FindMetaTable("Player")
 --- Returns whether a player has broken legs
 -- @realm shared
 function PLAYER:HasBrokenLegs()
-    return tobool(self:GetRelay("brokenLegs", false))
+    return self:GetRelay("brokenLegs", false)
+end
+
+--- Gets the time when the player's legs were broken
+-- @realm shared
+function PLAYER:GetBrokenLegsStartTime()
+    return self:GetRelay("brokenLegsStartTime", 0)
 end
--- gamemode/core/libs/sv_medical.lua
+++ gamemode/core/libs/sv_medical.lua
@@ -6,12 +6,10 @@ local PLAYER = FindMetaTable("Player")
 --- Breaks a player's legs
 -- @realm server
 function PLAYER:BreakLegs()
-    self.impulseBrokenLegsTime = CurTime() + (impulse.Config.BrokenLegsHealTime or 60)
-    self.impulseBrokenLegs = true
-
     if ( self:HasBrokenLegs() ) then return end
 
     self:SetRelay("brokenLegs", true)
+    self:SetRelay("brokenLegsStartTime", CurTime())
 
     self:EmitSound("impulse-reforged/bone" .. math.random(1, 3) .. ".wav")
     self:Notify("You have broken your legs and can barely move!", NOTIFY_ERROR)
@@ -22,7 +20,6 @@ end
 --- Fixes a player's legs
 -- @realm server
 function PLAYER:FixLegs()
-    self:SetRelay("brokenLegs", false)
-    self.impulseBrokenLegs = false
-    self.impulseBrokenLegsTime = nil
+    self:SetRelay("brokenLegs", nil)
+    self:SetRelay("brokenLegsStartTime", nil)
 end
