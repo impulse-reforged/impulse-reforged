ffd4db9c2c982542b578da4f488f91c37540e629
Riggs <riggs9162@gmx.de>
2025-12-10 01:03:06 +0100

    feat(ldoc): add ldoc.bat

---
+++ ldoc.bat
@@ -0,0 +1,5 @@
+@echo off
+SET "LUA_PATH=A:\LuaRocks\lua\?.lua;A:\LuaRocks\lua\?\init.lua;A:\LuaRocks\?.lua;A:\LuaRocks\?\init.lua;A:\LuaRocks\..\share\lua\5.4\?.lua;A:\LuaRocks\..\share\lua\5.4\?\init.lua;.\?.lua;.\?\init.lua;C:\Users\Riggs\AppData\Roaming\luarocks\share\lua\5.4\?.lua;C:\Users\Riggs\AppData\Roaming\luarocks\share\lua\5.4\?\init.lua"
+SET "LUA_CPATH=A:\LuaRocks\?.dll;A:\LuaRocks\..\lib\lua\5.4\?.dll;A:\LuaRocks\loadall.dll;.\?.dll;C:\Users\Riggs\AppData\Roaming\luarocks\lib\lua\5.4\?.dll"
+lua S:\SteamLibrary\steamapps\common\GarrysMod\garrysmod\gamemodes\impulse-reforged\ldoc\ldoc.lua %*
+pause
